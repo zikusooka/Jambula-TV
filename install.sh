05c152d92dd8da4984f012cb1ac5a0a4d8d10c6b
Jambula Labs <tv@jambula.net>
2015-06-06 15:08:29 +0300

    Prepare for initial public release

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+PROJECT_NAME=JambulaTV
+SOURCESDIR=/opt
+PROJECT_BASE_DIR=$SOURCESDIR/$PROJECT_NAME
 
 # Source functions file
-. /JambulaTV/RELEASE-2.0/functions
+. $PROJECT_BASE_DIR/functions
 
 
 
@@ -9,11 +12,14 @@
 #  MAIN SCRIPT  #
 #################
 
+cat <<EOF > /dev/null 
+# Install custom kernel
+kernel_install 
+
 # Create installation directory
 install_dir_create
 
 # pick desired X11 video driver
-#cat <<EOF > /dev/null 
 case $X11_DRIVER in
 intel)
 intel_x11_driver_install uxa # Add Video Acceleration as argument
@@ -31,8 +37,7 @@ openchrome_x11_driver_install
 echo "Using default X11 driver ..."
 ;;
 esac
-#EOF
-#
+
 # Sound libraries
 alsa_lib_install
 alsa_utils_install
@@ -49,17 +54,19 @@ pyalsaaudio_install
 pulseaudio_configure
 portaudio_install
 oss_devices_configure
-SDL1_install
+SDL_install
 openal_soft_install
 ffmpeg_deps_install
 live555_install
 libdvbpsi_install 
 #
 # Install ffmpeg first without x264 - will be removed and re-installed shortly
-ffmpeg_install && x264_install 
+#ffmpeg_install && x264_install 
 # Uninstall already existing ffmpeg (without x264 libs) and install agian w/ x264
-uninstall_src_pkgs ffmpeg && ffmpeg_install
+#uninstall_src_pkgs ffmpeg && ffmpeg_install
 #
+ffmpeg_install
+x264_install
 vlc_install
 xine_lib_install
 xine_ui_install
@@ -69,40 +76,74 @@ v4l_utils_install
 tvheadend_install
 tvheadend_configure
 fmtools_install
-libusb_install
-xbmc_deps_install
-xbmc_install
-xbmc_addons_pvr_install
-xbmc_addons_all_install
-python_xbmc_install
-xbmc_configure
-xbmc_customization 
 
-# kodi
-# ----
-#kodi_install
+# Kodi (formerly XBMC)
+kodi_deps_install
+kodi_install
+kodi_configure
+kodi_customization 
+kodi_platform_install # Needed for PVR and Some Addons
+kodi_pvr_hts_install
+kodi_addons_install
 
-lircd_configure
-x11_configure
-autologin_lxde
+# Tools used by Kodi
+kodi_lircd_configure
+kodi_osd_configure 
+remind_configure 
+
+# X11 setup
+x11_server_configure
+x11_superuser_permissions_configure
+x11_user_autologin_lxde_configure
+
+# Boot screen customization
 plymouth_customization
-grub_settings 
-autologin_user_configure
-autostart_xbmc_service
-tv_cards_configure
-tv_pulse_audio_configure 
+grub_settings
 
+# Auto start for JambulaTV
+kodi_jambulatv_service_configure
+
+# -----------------
 # System Settings
+# -----------------
 boot_optimization
-network_configure
+wired_interfaces_configure
+wireless_interfaces_configure
 openssl_configure
 wol_configure 
+beep_spkr_configure # Alert sounds - beep2_install no longer needed
+# Run these before icinga configure
+chrony_configure
+dnsmasq_configure
+openresolv_install
+openresolv_configure
 
-# Desktop
+# -----------------
+# Internet Access 
+# -----------------
+# Dual virtual interfaces
+virtual_wifi_interfaces_configure
+internet_mifi_configure # Wifi (mifi)
+internet_usb_3g_configure # USB (3G)
+
+# Proxy server support
+ziproxy_install
+ziproxy_configure
+
+# WAN Accelerator support
+squid_install # Takes a very long time ~ 50 mins
+squid_configure # configured for use by ziproxy WAN accelerator
+ziproxy_wan_accel_configure
+
+# ----------------
+# Desktop Access
+# ----------------
 vncserver_configure
 firefox_install
 
+# --------------------
 # Web/GUI Interfaces
+# --------------------
 nginx_install
 nginx_configure
 nginx_customization
@@ -110,7 +151,14 @@ php_configure
 nginx_fcgiwrap_install
 nginx_spawn_fcgi_configure
 
-# HotSpot settings
+# ----------
+# Databases
+# ----------
+mysqld_configure
+
+# --------------------------------
+# Wireless Access Point (HotSpot)
+# --------------------------------
 freeradius_install
 freeradius_configure
 freeradius_attributes_configure
@@ -120,113 +168,82 @@ coova_chilli_install
 haserl_install
 coova_chilli_configure
 coova_chilli_customization
+coova_chilli_icinga2_dns_monitor
 hostapd_install
 hostapd_configure
-# Run these before icinga configure
-chrony_configure
-dnsmasq_configure
-openresolv_install
-openresolv_configure
 
-# Proxy server support
-ziproxy_install
-ziproxy_configure
-
-# WAN Accelerator support
-squid_install
-squid_configure # configured for use by ziproxy WAN accelerator
-ziproxy_wan_accel_configure
-
-# Monitoring software
+#------------
+# Monitoring
+# -----------
 icinga2_install # Takes a very long time ~ 1 hour
 monitoring_plugins_install
 icinga2_ido_mysql
 icinga2_configure
 icingaweb2_install
+icingaweb2_configure
 
-# CCTV Support
-zoneminder_pre_install
-zoneminder_install 
-zoneminder_configure 
+# --------
+# Content
+# --------
 
-# Internet Access 
-# Dual virtual interfaces
-virtual_wifi_interfaces_configure
-# Wifi (mifi)
-internet_mifi_configure 
-# USB (3G) 
-internet_usb_3g_configure
+# DLNA/UPnP
+xupnpd_install
+xupnpd_configure
+# minidlna: No support for playlists, live TV
+#minidlna_install
+#minidlna_configure
 
-# FlexGet
+# VoD Streaming
+video_on_demand_configure
+
+# Auto download of TV Shows, Movies, and Podcasts
 flexget_deps_install
 flexget_install
 flexget_configure
 
 # Torrents
+xmlrpc_install
+libtorrent_install
 rtorrent_install
 rtorrent_configure
 rtgui_configure
 
-# Aria2
-aria2_install
+# Downloads
+aria2_install # Takes ~ 40 mins
 aria2_configure
 
+# File server
+samba_configure
 
-# Alert sounds
-beep_spkr_configure
-#beep2_install # No longer needed
+# ---------------------
+# Security Monitoring
+# ---------------------
+zoneminder_pre_install
+zoneminder_install
+zoneminder_configure 
+zoneminder_zms_inetd
 
-# DLNA/UPnP
-# ---------
-# xupnp: Supports playlists, live TV
-xupnpd_install
-xupnpd_configure
-
-# minidlna: No support for playlists
-#minidlna_install
-#minidlna_configure
-
-# VoD Streaming
-video_tv_on_demand_configure 
-#tv_on_demand_configure 
-
-# Reminders - Birthdays, Anniversaries, Holidays, etc
-remind_configure 
-
-# Email - Phone home support
-email_smtp_install
-email_smtp_configure
-
-
+# -----------------
 # Utilities/tools
-# ---------------
-# DVB-T/S
+# -----------------
 w_scan_install 
-# Youtube-dl
 youtube_dl_install
-# Cut TCP/IP connections via gateway
 cutter_install 
-# IPTV
 rtmpdump_install
-# GSM Phone Access
 gnokii_install
 gammu_install
-
-
-# Google SMS Support
+email_smtp_install
+email_smtp_configure
 gdata_install
 google_sms_setup
- 
-# Remote Access
 reverse_ssh_configure
 
-# VPN Support
-openvpn_configure 
-
-# IM Chat Server (Needed by XMPP useage in Asterisk)
+# ----------------
+# Communications 
+# ----------------
+# IM Chat Server (Needed by XMPP usage in Asterisk)
 jabberd_configure
-jabberd_add_users 
-
+jabberd_add_users
 
 # Telephone system support
 dahdi_install
@@ -234,12 +251,13 @@ libpri_install
 asterisk_install
 asterisk_configure 
 
-chan_mobile_configure
+# XMPP
+chan_motif_configure
 
+# GSM Trunks/ Mobile Gateways
+chan_mobile_configure
 chan_dongle_install 
 chan_dongle_configure
-
-chan_motif_configure
 
 # Cepstral Support # Not Free!
 if [ "$CEPSTRAL_EULA" = "agree" ];
@@ -254,17 +272,27 @@ fi
 sphinxbase_install
 pocketsphinx_install
 
-# UniMRCP
-#unimrcp_deps_install 
-#unimrcp_install
-#asterisk_unimrcp_install
-
+# ----------------
+# Administration 
+# ----------------
 # Cloud and Backup support
 owncloud_install
 
 
+# ---------
+# Notices
+# ---------
 # Post install Notes
 post_install_notice
+#
+EOF
 
-# END #
-#######
+# ---------
+# The End
+# ---------
+
+
+
+
+
+exit
