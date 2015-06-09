bba6695b041cd8245ecbbe7d2b523e56813d3753
Jambula Labs <tv@jambula.net>
2015-06-09 15:24:17 +0300

    Add jambulatv-dvbt-scan script used to scan 4 DVB-T services

---
+++ workspace.REMOVE/testing.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+PROJECT_NAME=JambulaTV
+SOURCESDIR=/opt
+PROJECT_BASE_DIR=$SOURCESDIR/$PROJECT_NAME
+
+# Source functions file
+. $PROJECT_BASE_DIR/functions
+
+
+
+#################
+#  MAIN SCRIPT  #
+#################
+
+
+
+
