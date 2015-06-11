5307ffed1b2c2c2664cec0569637ff224adda724
Jambula Labs <tv@jambula.net>
2015-06-11 17:26:25 +0300

    Add reminders timer for use with reminders.service - remove old way of handling reminders

--- configs/kodi/autoexec.py
+++ configs/kodi/autoexec.py
@@ -5,8 +5,9 @@ import subprocess
 import fnmatch
 import time
 #
+#
 # Wait for OSD to be fully initialized, since we need to notify on display
 time.sleep(30) 
-#
+
 # Reminders: Birthdays, Anniversaries, Holidays, etc
-subprocess.check_call(['/usr/bin/jambulatv-remind'])
+# subprocess.check_call(['/usr/bin/jambulatv-remind']) # Not needed:- see reminders.timer
