bac8731bde241d2989b1527108cfb95c5315f48c
Jambula Labs <tv@jambula.net>
2015-09-19 15:44:20 +0300

    CoovaChilli: Fix patch for config-local.sh file - Some jambula changes had been rejected

--- workspace.REMOVE/testing.sh
+++ workspace.REMOVE/testing.sh
@@ -11,4 +11,10 @@ PROJECT_BASE_DIR=$SOURCESDIR/$PROJECT_NAME
 #################
 #  MAIN SCRIPT  #
 #################
+# Patch logn-config.sh
+patch -p1 $COOVA_HTML_DIR/www/config-local.sh < $PROJECT_PATCHES_DIR/coova_login_config_local.patch  
 
+# patch login.chi: Redirect to customized Jambula Splash Web Pages
+patch -p1 $COOVA_HTML_DIR/www/login.chi < $PROJECT_PATCHES_DIR/coova_login_chi.patch
+#
+#
