074c2b5149b327c4007fad1e3b7c735b2a8ba4b9
Jambula Labs <tv@jambula.net>
2015-12-12 17:25:18 +0300

    Coova-Chilli: Work around Sign-in to Network issue on Android plus add/update landing pages

---
+++ customize/coova/html/landing/scripts/timer_30.js
@@ -0,0 +1,14 @@
+(function () {
+    var timeLeft = 30,
+        cinterval;
+
+    var timeDec = function (){
+        timeLeft--;
+        document.getElementById('countdown').innerHTML = timeLeft;
+        if(timeLeft === 0){
+            clearInterval(cinterval);
+        }
+    };
+
+    cinterval = setInterval(timeDec, 1000);
+})(); 
