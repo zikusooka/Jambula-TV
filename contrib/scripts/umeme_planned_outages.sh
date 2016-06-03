871eed090aa6449e6f6ce89ebdd32397dca2f1cf
Jambula Labs <tv@jambula.net>
2016-06-03 21:10:19 +0300

    Update UMEME planned outages contributed script for June 2016 - Filename keeps changing - Use search function at UMEMEME website

--- contrib/scripts/umeme_planned_outages.sh
+++ contrib/scripts/umeme_planned_outages.sh
@@ -3,6 +3,9 @@
 # Prerequisites:
 # Install catdoc i.e. yum install catdoc
 
+# Future enhancements
+# 1. Send Telegram Notices
+
 DATE_AFTER_TOMORROW_UMEME_FORMAT=$(date --date='2 day' +'%A,%d')
 DATE_TOMORROW_UMEME_FORMAT=$(date --date='1 day' +'%A,%d')
 DATE_TODAY_UMEME_FORMAT=$(date +'%A,%d')
@@ -14,17 +17,22 @@ TERM=xterm
 OSD_CMD="/usr/bin/jambulatv-osd"
 OSD_CMD_OPTS="-m"
 
-UMEME_RESOURCES_URL="http://www.umeme.co.ug/assets/resources"
-UMEME_PLANNED_OUTAGE_FILE="Planned%20Works%20$(date +%B)%20$(date '+%Y').xls"
-UMEME_EXCEL_FILE=/tmp/UMEME_Planned.$(date +%B).xls
-UMEME_MONTHLY_FILE=/JambulaTV/Downloads/UMEME_Planned.$(date +%B).csv
+THIS_MONTH=$(date +%B)
 
-VILLAGE="$@"
+UMEME_ROOT_URL=http://www.umeme.co.ug
+UMEME_OUTAGES_URL=$(curl -s $UMEME_ROOT_URL/search.html?id=7 -d search=Planned%20Outages | grep extract | grep '.xls' | sort -u | sed 's:<[^>]\+>::g' | sed 's:^ *::g' | grep -i "$THIS_MONTH" | sed 's: :%20:g')
 
-if [ "x$VILLAGE" = "x" ];
+UMEME_PLANNED_OUTAGE_FILE="$(basename $UMEME_OUTAGES_URL)"
+
+UMEME_EXCEL_FILE=/tmp/UMEME_Planned.${THIS_MONTH}.xls
+UMEME_MONTHLY_FILE=/JambulaTV/Downloads/UMEME_Planned.${THIS_MONTH}.csv
+
+LIST_OF_AREAS="$@"
+
+if [ "x$LIST_OF_AREAS" = "x" ];
 then
 clear
-echo "Usage: $(basename $0) [VILLAGE]"
+echo "Usage: $(basename $0) [LIST_OF AREAS (Seperate with space e.g. Ntinda Kiwatule)]"
 exit
 fi
 
@@ -33,12 +41,13 @@ fi
 host -W 1 google.com 8.8.8.8 > /dev/null 2>&1
 INTERNET_ALIVE=$?
 
-# Download UMEME file for this month if it does not exist and Internet is up
-if [ ! -e "$UMEME_MONTHLY_FILE" ] && [ "$INTERNET_ALIVE" = "0" ];
+# Download UMEME file for this month if Internet is up
+if [ "$INTERNET_ALIVE" = "0" ];
 then
 # pick latest from Internet
 echo "Downloading UMEME Planned outages file [$UMEME_PLANNED_OUTAGE_FILE] for this month ..."
-wget -c -O $UMEME_EXCEL_FILE $UMEME_RESOURCES_URL/$UMEME_PLANNED_OUTAGE_FILE
+wget -c -O $UMEME_EXCEL_FILE $UMEME_OUTAGES_URL
+
 	if [ "$?" != "0" ];
 	then
 	echo "Error: Failed to download the file [$UMEME_PLANNED_OUTAGE_FILE]"
@@ -53,12 +62,15 @@ then
 # No file, and no Internet, so quit
 echo "The file $UMEME_MONTHLY_FILE is missing.  Please download it at:
 
-$UMEME_RESOURCES_URL/$UMEME_PLANNED_OUTAGE_FILE
+$UMEME_ROOT_URL
 "
 exit
 fi
 
 
+# Check for selected areas
+for VILLAGE in $LIST_OF_AREAS
+do
 
 # Two days from now
 grep -i $DATE_AFTER_TOMORROW_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
@@ -74,3 +86,5 @@ then
 grep -i $DATE_TODAY_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
    $OSD_CMD $OSD_CMD_OPTS "UMEME planned outage: There will be no power in $VILLAGE Today"
 fi
+
+done
