619313133efd2ee0675caa4318d11f2a62a2e8b2
Jambula Labs <tv@jambula.net>
2016-06-06 13:38:31 +0300

    Flexget: Add spool directory for podcasts.  Make variables.yml template dynamic - Add time variables

--- workspace.REMOVE/testing.sh
+++ workspace.REMOVE/testing.sh
@@ -6,6 +6,16 @@ PROJECT_BASE_DIR=$SOURCESDIR/$PROJECT_NAME
 # Source functions file
 . $PROJECT_BASE_DIR/functions
 
+# My credentials - Remove before shipping!!!
+TRAKT_USERNAME=dc8982
+TRAKT_WATCHLIST=JambulaTV
+TORRENTS_DOMAIN_NAME=JambulaTV
+GOOGLE_SERVICES_USERNAME="smtprelayug@gmail.com"
+GOOGLE_SERVICES_PASSWORD="256smtprelayug"
+EMAIL_TO_ADDRESS="joseph@zikusooka.com"
+TELEGRAM_API_BOT="189563360:AAFFiDHQXgBTLSPsdVbXDbMkG-khkIGlqa8"
+TELEGRAM_USERNAME="jlabs"
+
 
 
 #################
