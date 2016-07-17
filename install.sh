2064c2234de32709c1828d32bebd233f24bf8c98
Jambula Labs <tv@jambula.net>
2016-07-17 10:41:17 +0300

    Updated functions file and set WWW user and group settings only after nginx has been installed for first time runs.  Remove ICINGA2_WEB_USER and ASTERISK_FILES_GROUP variables

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ PROJECT_BASE_DIR=$SOURCESDIR/$PROJECT_NAME
 #################
 #  MAIN SCRIPT  #
 #################
+kernel_install 
 cat > /dev/null << EOF
 # Install custom kernel
 kernel_install 
