7b539a008571b3f4a14e87cbf3e6f875f7134607
Jambula Labs <tv@jambula.net>
2016-07-24 16:00:18 +0300

    JambulaTV: Disabled timer/sleep function in kodi autoexec.py startup script

--- configs/kodi/autoexec.py
+++ configs/kodi/autoexec.py
@@ -7,7 +7,7 @@ import time
 #
 #
 # Wait for OSD to be fully initialized, since we need to notify on display
-time.sleep(30) 
+#time.sleep(30) 
 
 # Reminders: Birthdays, Anniversaries, Holidays, etc
 # subprocess.check_call(['/usr/bin/jambulatv-remind']) # Not needed:- see reminders.timer
