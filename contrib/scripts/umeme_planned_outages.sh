512701ed8da96c2b5092cfc239d87b6c20b42af0
Jambula Labs <tv@jambula.net>
2016-08-16 01:04:51 +0300

    JambulaTV: Added Telegram notification support to umeme planned outages contributed script. Fixed big in searching for monthly excel file

--- contrib/scripts/umeme_planned_outages.sh
+++ contrib/scripts/umeme_planned_outages.sh
@@ -17,7 +17,7 @@ TERM=xterm
 OSD_CMD="/usr/bin/jambulatv-osd"
 OSD_CMD_OPTS="-m"
 
-THIS_MONTH=$(date +%B)
+THIS_MONTH=$(date +%b)
 
 UMEME_ROOT_URL=http://www.umeme.co.ug
 UMEME_OUTAGES_URL=$(curl -s $UMEME_ROOT_URL/search.html?id=7 -d search=Planned%20Outages | grep extract | grep '.xls' | sort -u | sed 's:<[^>]\+>::g' | sed 's:^ *::g' | grep -i "$THIS_MONTH" | sed 's: :%20:g')
@@ -27,15 +27,38 @@ UMEME_PLANNED_OUTAGE_FILE="$(basename $UMEME_OUTAGES_URL)"
 UMEME_EXCEL_FILE=/tmp/UMEME_Planned.${THIS_MONTH}.xls
 UMEME_MONTHLY_FILE=/JambulaTV/Downloads/UMEME_Planned.${THIS_MONTH}.csv
 
+TELEGRAM_CREDENTIALS_CONFIG=/etc/JambulaTV/messaging-telegram.cfg
+
 LIST_OF_AREAS="$@"
 
+
+
+###############
+#  FUNCTIONS  #
+###############
+
+send_telegram () {
+. $TELEGRAM_CREDENTIALS_CONFIG
+# SendMesage
+$TELEGRAM_SCRIPT SendMessage "$@" &
+}
+
+usage () {
 if [ "x$LIST_OF_AREAS" = "x" ];
 then
 clear
 echo "Usage: $(basename $0) [LIST_OF AREAS (Seperate with space e.g. Ntinda Kiwatule)]"
 exit
 fi
+}
 
+
+
+#################
+#  MAIN SCRIPT  #
+#################
+
+usage
 
 # Check if Internet connection is up
 host -W 1 google.com 8.8.8.8 > /dev/null 2>&1
@@ -73,18 +96,36 @@ for VILLAGE in $LIST_OF_AREAS
 do
 
 # Two days from now
+# -----------------
+# OSD
 grep -i $DATE_AFTER_TOMORROW_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
    $OSD_CMD $OSD_CMD_OPTS "UMEME planned outage: There will be no power in $VILLAGE on $DATE_AFTER_TOMORROW_DAY_FORMAT"
 
+# Telegram
+grep -i $DATE_AFTER_TOMORROW_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
+	send_telegram "UMEME planned outage: There will be no power in $VILLAGE on $DATE_AFTER_TOMORROW_DAY_FORMAT"
+
 # Tomorrow
+# ---------
+# OSD
 grep -i $DATE_TOMORROW_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
    $OSD_CMD $OSD_CMD_OPTS "UMEME planned outage: There will be no power in $VILLAGE on $DATE_TOMORROW_DAY_FORMAT"
 
+# Telegram
+grep -i $DATE_TOMORROW_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
+	send_telegram "UMEME planned outage: There will be no power in $VILLAGE on $DATE_TOMORROW_DAY_FORMAT"
+
 # Today before 9am
+# ----------------
+# OSD
 if [ "$(date +%H)" -lt "12" ];
 then
 grep -i $DATE_TODAY_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
    $OSD_CMD $OSD_CMD_OPTS "UMEME planned outage: There will be no power in $VILLAGE Today"
+
+# Telegram
+grep -i $DATE_TODAY_UMEME_FORMAT $UMEME_MONTHLY_FILE | grep -i "$VILLAGE" > /dev/null 2>&1 && \
+	send_telegram "UMEME planned outage: There will be no power in $VILLAGE Today"
 fi
 
 done
