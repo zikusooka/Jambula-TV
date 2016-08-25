02567badf278be1b03e632c2027fb75d52633702
Jambula Labs <tv@jambula.net>
2016-08-25 23:36:08 +0300

    JambulaTV: Modified configure functions for initial setups.  Fixed typo in setup completed file in jambulatv-initial-setup script

--- customize/setup/form_01.php
+++ customize/setup/form_01.php
@@ -16,10 +16,16 @@
  $_SESSION['post'][$key] = $value;
  } 
 
+ $fname=".initial-setup-completed";
+ $file=fopen($fname,"w");
+ fwrite($file,"Setup Done");
+ fclose($file);
+
  echo '<p><span id="success">Thank you for completing setup.  Enjoy!</span></p>';
- echo '<p>Please download the Remote App as follows</p>';
- echo '<p><a href="https://play.google.com/store/apps/details?id=org.leetzone.android.yatsewidgetfree">Android</a></p>';
- echo '<p><a href="https://itunes.apple.com/en/app/official-kodi-remote/id520480364?mt=8">iOS</a></p>';
+ echo '<p></p>';
+ echo '<p>Please download the Remote App as follows:</p>';
+ echo '<h2><a href="https://play.google.com/store/apps/details?id=org.leetzone.android.yatsewidgetfree">Android</a></h2>';
+ echo '<h2><a href="https://itunes.apple.com/en/app/official-kodi-remote/id520480364?mt=8">iOS</a></h2>';
 
  ?>
  </div>
