1293f73b313796565cfca980c63f4f8b6bb9812d
Jambula Labs <tv@jambula.net>
2016-12-15 13:22:05 +0300

    JambulaTV: Made major additions and improvements to initial setup
    * Moved setup of user credentials away from install functions to initial setup script
    
    * Added setup functions for email, gmail_smtp, telegram, whatsapp
    
    * Added MySQL query to jambulatv setup tables
    
    * Added setup functions for flexget secrets in variables.yml
    
    * Also added rtorrent notify_via_email and asterisk_google_xmpp credentials setups
    
    * Removed sending of SMS using google in reverse-ssh-setup.  Not working anymore
    
    * Removed no longer needed variables from functions file

--- install.sh
+++ install.sh
@@ -343,7 +343,6 @@ home_automation_scripts
 # ------------------------
 # Notifications/ Messaging
 # ------------------------
-email_messaging_configure
 sleekxmpp_install 
 python_telegram_bot_install
 telegram_messaging_configure
