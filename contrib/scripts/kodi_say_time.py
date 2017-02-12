e494d8c0a9bdb8f92db083a84a1308e375e9a75c
Jambula Labs <tv@jambula.net>
2017-02-12 19:32:34 +0300

    JambulaTV: Assign a 'say current time' lirc button to physical remote
    * Added RunScript action 'kodi_say_time.py' to KEY_GOTO button on remote
      i.e. Updated Lircmap and remote.xml files
    
    * Renamed util/pip.py to kodi_activate_pip.py and moved to contributed scripts directory
    
    * Add copy kodi_*py scripts from contributed dir to kodi_scripts configuration functions
    
    * Added kodi_say_time.py script to contributed scripts directory
    
    * Added pause_player function to jambulatv-kodi-controller

---
+++ contrib/scripts/kodi_say_time.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Ask for current time
+
+__author__ = 'JambulaTV'
+
+import subprocess
+import sys
+
+
+
+# Pause Player
+subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause'])
+
+# Say current time
+subprocess.check_call(['/usr/bin/jambulatv-text2speech', 'time'])
+
+# Un Pause Player
+subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause'])
