06e3d6c003cee68f350e68543fadac14f4d21f88
Jambula Labs <tv@jambula.net>
2017-02-28 16:58:50 +0300

    JambulaTV:  Modify play_pause function in kodi controller script to allow pausing with   either replay (toggle) or no replay ability
    * Add noreplay option to kodi_say_time.py script for telling time.  If player was already
      paused, then dont play before saying the time

--- contrib/scripts/kodi_say_time.py
+++ contrib/scripts/kodi_say_time.py
@@ -9,7 +9,7 @@ import sys
 
 
 # Pause Player
-subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause'])
+subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause', 'noreplay'])
 
 # Say current time
 subprocess.check_call(['/usr/bin/jambulatv-text2speech', 'time'])
