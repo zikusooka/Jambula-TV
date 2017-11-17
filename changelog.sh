cc4ac1d80fc7d0c68d1319caeb2428b36c9c71e9
Jambula Labs <tv@jambula.net>
2017-11-17 13:55:33 +0300

    JambulaTV: Add changelog.sh script - Useful for generating changes between tagged versions

---
+++ changelog.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+SOURCESDIR=/opt
+PROJECT_NAME=JambulaTV
+PROJECT_BASE_DIR=$SOURCESDIR/$PROJECT_NAME
+
+ACTION=$1
+SPECIFIED_FROM_TAGGED_VERSION=$2
+SPECIFIED_TO_TAGGED_VERSION=$3
+LAST_TAGGED_VERSION=$(git describe --tags --abbrev=0)
+
+
+
+# Change to git directory
+#cd $PROJECT_BASE_DIR
+
+case $ACTION in
+since-last-version)
+# Changes from Last tagged version up to current HEAD
+git log --decorate ${LAST_TAGGED_VERSION}..HEAD
+;;
+
+since-specific-version)
+if [[ "x$SPECIFIED_FROM_TAGGED_VERSION" = "x" ]];
+then
+clear
+cat <<EOF
+Usage: $(basename $0) $ACTION [LAST VERSION]
+EOF
+exit 1
+fi
+
+# changes since specified-from-tag to current HEAD
+git log --decorate ${SPECIFIED_FROM_TAGGED_VERSION}..HEAD
+;;
+
+between-specific-versions)
+if [[ "x$SPECIFIED_FROM_TAGGED_VERSION" = "x" || "x$SPECIFIED_TO_TAGGED_VERSION" = "x" ]];
+then
+clear
+cat <<EOF
+Usage: $(basename $0) $ACTION [FROM VERSION] [TO VERSION]
+EOF
+exit 1
+fi
+
+# changes since specified-from-tag to specified-to-tag
+git log --decorate ${SPECIFIED_FROM_TAGGED_VERSION}..${SPECIFIED_TO_TAGGED_VERSION}
+;;
+
+*)
+clear
+cat <<EOF
+Usage: $(basename $0) [ since-last-version | since-specific-version | between-specific-versions ]
+EOF
+exit 1
+;;
+esac
