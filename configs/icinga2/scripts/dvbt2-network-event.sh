6dbd690940ac3e18e7c1af5f78b170ea5d04924c
Jambula Labs <tv@jambula.net>
2018-02-27 00:58:09 +0300

    JambulaTV:  Add DVB-T/T2 repair functions to jambulatv-tvheadend-controller tool             i.e. dvb_tvheadend_network_repair
    * Assign the bulk of tasks performed by icinga2 script: dvbt2-network-event.sh to
      the command 'jambulatv-tvheadend-controller dvb repair' instead
    
    * Also fix contributed icinga2 plugin: check_dvbt2_network to match above changes
    
    * Modify and move OSD script notices to top of this tool
    
    * Add function for getting last known tvheadend channel number
    
    * Fix issue in which 'find' command fails to properly detect corrupted dvb network
      config files i.e. under configure_dvb_network function
    
    * Rename 'dvb_network_add' function to 'dvb_tvheadend_network_add'
    
    * Add 'just booted" warning notice to icinga2 contributed plugin: check_systemd

--- configs/icinga2/scripts/dvbt2-network-event.sh
+++ configs/icinga2/scripts/dvbt2-network-event.sh
@@ -4,61 +4,11 @@
 # Plus ensure that DVBT network in tvheadend exists and is setup properly
 #
 # Variables
-PROJECT_NAME=JambulaTV
-PROJECT_SYSTEM_CONF_DIR=/etc/$PROJECT_NAME
-PROJECT_FUNCTIONS_FILE=$PROJECT_SYSTEM_CONF_DIR/functions
-PROJECT_DVBT_SETTINGS_FILE=$PROJECT_SYSTEM_CONF_DIR/dvbt-settings.cfg
-
-# Source install functions
-. $PROJECT_FUNCTIONS_FILE
-
-# Source DVBT settings file
-. $PROJECT_DVBT_SETTINGS_FILE
-
-# Other variables
-PVR_STATUS_FILE=/tmp/pvr_status # Use the same file in '/usr/bin/jambulatv-kodi-controller'
-PVR_STATUS_CMD="/usr/bin/jambulatv-kodi-controller addon_status pvr.hts"
-#
-# DVB-T Tuner node
-TVHEADEND_DVBT_TUNER_NODE=/dev/dvb/adapter$DVBT_TUNER_NO
-# See if DVBT network UUID exists
-TVHEADEND_DVBT_NETWORK_UUID=$(curl -s http://127.0.0.1:9981/api/mpegts/network/grid?limit=100000 | jq '.entries[] | select(.networkname=="'"$DVBT_NETWORK_NAME"'") |.uuid' | sed 's:"::g')
-# Check for non-set-channels
-curl -s http://127.0.0.1:9981/api/channel/list?limit=10000 | jq '.entries[]' | grep 'name-not-set' > /dev/null 2>&1
-NONE_SET_CHANNELS_FOUND=$?
-# Notices
-OSD_NOTICE_NO_TUNER="Error:  Unable to detect the TV Tuner. This is most likely a hardware issue.  Please check your DVB-T2 tuner and ensure it is properly connected"
-OSD_NOTICE_NO_NETWORK="Error:  The TV Channels list is corrupted. This may be due to a bad shutdown previously. I will attempt to correct it now, please try again shortly"
+REPAIR_COMMAND="/usr/bin/jambulatv-tvheadend-controller dvb repair"
 
 
 
 #################
 #  MAIN SCRIPT  #
 #################
-
-#  PVR Status Query
-$PVR_STATUS_CMD > /dev/null 2>&1
-# Pull Status
-. $PVR_STATUS_FILE
-#
-# Quit if Kodi PVR Manager is not enabled
-[ "$ADDON_STATUS" = "true" ] || exit 0
-
-# If no DVB-T2 tuner was found
-if [[ ! -e "$TVHEADEND_DVBT_TUNER_NODE" ]];
-then
-# OSD notice
-sudo /usr/bin/jambulatv-osd -m "$OSD_NOTICE_NO_TUNER"
-# CLI notice
-clear && echo "$OSD_NOTICE_NO_TUNER"
-# If DVBT Network was accidentally wiped out
-elif [[ "$NONE_SET_CHANNELS_FOUND" = "0" && "x$TVHEADEND_DVBT_NETWORK_UUID" = "x" ]];
-then
-# OSD notice
-sudo /usr/bin/jambulatv-osd -m "$OSD_NOTICE_NO_NETWORK"
-# CLI notice
-clear && echo "$OSD_NOTICE_NO_NETWORK"
-
-# Restore DVB-T network
-sudo /usr/bin/jambulatv-tvheadend-controller dvb all
-fi
+sudo $REPAIR_COMMAND
