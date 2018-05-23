12bd92ea9465fc1849a65008e04160bfd67b897d
Jambula Labs <tv@jambula.net>
2018-05-23 12:19:40 +0300

    JambulaTV: Update kodi scripts used to say the Time and Weather            These scripts are called when using an IR remote
    * Make the Text-to-Speech commands contained in these 2 scripts
      i.e. kodi_say_time.py and kodi_say_weather.py conform to the
      new CLI command ordering of options and arguments
    
    * Add support for specifying which Text-to-Speech API service is
      used

--- contrib/scripts/kodi_say_time.py
+++ contrib/scripts/kodi_say_time.py
@@ -6,13 +6,11 @@ __author__ = 'JambulaTV'
 import subprocess
 import sys
 
-
+SPEECH_API = '2'
 
 # Pause Player
 subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause', 'noreplay'])
-
 # Say current time
-subprocess.check_call(['/usr/bin/jambulatv-text2speech', 'time'])
-
+subprocess.check_call(['/usr/bin/jambulatv-text2speech', SPEECH_API, 'time'])
 # Un Pause Player
 subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause'])
--- contrib/scripts/kodi_say_weather.py
+++ contrib/scripts/kodi_say_weather.py
@@ -6,12 +6,11 @@ __author__ = 'JambulaTV'
 import subprocess
 import sys
 
+SPEECH_API = '1'
 
 # Pause Player
 subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause', 'noreplay'])
-
 # Say current time
-subprocess.check_call(['/usr/bin/jambulatv-text2speech', 'weather'])
-
+subprocess.check_call(['/usr/bin/jambulatv-text2speech', SPEECH_API, 'weather'])
 # Un Pause Player
 subprocess.check_call(['/usr/bin/jambulatv-kodi-controller', 'video_play_pause'])
