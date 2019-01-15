f5e91cb7bcab10e43082de2898e2cfa43b6e880c
Jambula Labs <tv@jambulatv.net>
2019-01-15 20:58:20 +0300

    JambulaTV: Upgrade Linux kernel (4.19 branch) to version 4.19.15

--- upgrade.sh
+++ upgrade.sh
@@ -272,7 +272,7 @@ live555_install
 #_install_pause_check_4_errors_ $PROJECT_ATTENDANT_USERNAME $PROJECT_ATTENDANT_HOSTNAME "Insert program name here"
 
 #upgrade_kernel 4.20
-#upgrade_kernel 4.19.14
+#upgrade_kernel 4.19.15
 #upgrade_kernel 4.4.166
 
 #upgrade_openssl
