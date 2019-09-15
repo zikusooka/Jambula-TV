243aac662437ad7a67e69a017f78a7eae8db7ebc
Jambula Labs <tv@jambulatv.net>
2019-09-15 03:53:01 +0300

    JambulaTV: Upgrade Linux kernel (4.19 branch) to version 4.19.72

--- upgrade.sh
+++ upgrade.sh
@@ -337,7 +337,7 @@ mopidy_install
 #_install_pause_check_4_errors_ $PROJECT_ATTENDANT_USERNAME $PROJECT_ATTENDANT_HOSTNAME "Insert program name here"
 
 #upgrade_kernel 4.20
-#upgrade_kernel 4.19.60
+#upgrade_kernel 4.19.72
 #upgrade_kernel 4.4.174
 
 #upgrade_openssl
