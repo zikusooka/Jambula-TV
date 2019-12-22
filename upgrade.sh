7ef191be0a7c930d15bc0cc97d70326a0524b998
Jambula Labs <tv@jambulatv.net>
2019-12-22 22:10:49 +0300

    JambulaTV: Upgrade Linux kernel (5.4 branch) to version 5.4.6

--- upgrade.sh
+++ upgrade.sh
@@ -337,7 +337,7 @@ mopidy_install
 #DEBUGGING
 #_install_pause_check_4_errors_ $PROJECT_ATTENDANT_USERNAME $PROJECT_ATTENDANT_HOSTNAME "Insert program name here"
 
-#upgrade_kernel 5.4.1
+#upgrade_kernel 5.4.6
 #upgrade_kernel 4.20
 #upgrade_kernel 4.19.89
 #upgrade_kernel 4.4.174
