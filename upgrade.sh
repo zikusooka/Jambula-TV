b640b738a22fd00dd40fd8fedc1ec21a277b90c4
Jambula Labs <tv@jambulatv.net>
2020-11-09 06:35:13 +0300

    JambulaTV: Upgrade Linux kernel to latest stable version: 5.9.6

--- upgrade.sh
+++ upgrade.sh
@@ -370,7 +370,7 @@ nginx_install
 #DEBUGGING
 #_install_pause_check_4_errors_ $PROJECT_ATTENDANT_USERNAME $PROJECT_ATTENDANT_HOSTNAME "Insert program name here"
 
-#upgrade_kernel 5.9.2
+#upgrade_kernel 5.9.6
 #upgrade_kernel 5.8.12
 #upgrade_kernel 5.7.1
 #upgrade_kernel 5.6.2
