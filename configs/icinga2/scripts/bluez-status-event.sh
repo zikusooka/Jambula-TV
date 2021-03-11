e3df129435f7aa108b7d6ff6813c94b2641591ea
Jambula Labs <tv@jambulatv.net>
2021-03-11 03:12:54 +0300

    JambulaTV: Update Icinga2 'bluez-status-event.sh' script i.e. Source            MQTT settings such as server IP/port and command/options            from our global settings file

--- configs/icinga2/scripts/bluez-status-event.sh
+++ configs/icinga2/scripts/bluez-status-event.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-
+#
+# Source global settings
+PROJECT_NAME=JambulaTV
+PROJECT_SYSTEM_CONF_DIR=/etc/$PROJECT_NAME
+PROJECT_GLOBAL_SETTINGS_FILE=$PROJECT_SYSTEM_CONF_DIR/global-settings.cfg
+. $PROJECT_GLOBAL_SETTINGS_FILE
+#
 # Variables
 BLUEZ_MAC_ADDRESS=$(echo "$HOSTOUTPUT" | grep -oP "(?<=\[).+?(?=\])")
-MQTT_BROKER_IP=127.0.0.1
-MQTT_BROKER_PORT=8552
-MQTT_PUBLISH_CMD=/usr/bin/mosquitto_pub
-MQTT_PUBLISH_OPTS="--quiet -h $MQTT_BROKER_IP -p $MQTT_BROKER_PORT"
 MQTT_TOPIC_PRESENCE_BLUEZ=JambulaTV/house/presence/bluez/$BLUEZ_MAC_ADDRESS
-
-
 
 # Publish status via MQTT
 if [[ "$HOSTSTATE" = "UP" ]];
