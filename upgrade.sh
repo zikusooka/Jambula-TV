707a60d38f9727a901437644bc3dc651c2bf57ed
Jambula Labs <tv@jambulatv.net>
2021-08-03 14:14:51 +0300

    JambulaTV: Upgrade system's Linux kernel to latest version: 5.4.137
    * Update the .config file for Linux kernel 5.4.x series
    
    * Update other Longterm Linux kernel sources to recent versions

--- upgrade.sh
+++ upgrade.sh
@@ -378,7 +378,7 @@ v4l_utils_install
 #_install_pause_check_4_errors_ $PROJECT_ATTENDANT_USERNAME $PROJECT_ATTENDANT_HOSTNAME "Insert program name here"
 
 #upgrade_kernel 5.10.43
-#upgrade_kernel 5.4.125
+#upgrade_kernel 5.4.137
 #upgrade_kernel 4.19.194
 #upgrade_kernel 4.14.236
 #upgrade_kernel 4.9.272
