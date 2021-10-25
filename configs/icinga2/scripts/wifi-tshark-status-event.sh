d085b8866a4f5293a51ae5810712127455987e00
Jambula Labs <tv@jambulatv.net>
2021-10-25 22:41:41 +0300

    JambulaTV: Add support for wireless (WiFi) presence detection using            Icinga2 monitoring software
    * Add new icinga2 plugin named 'check_tshark' to contributed plugins
      directory. This tool is responsible for scanning and detection of
      specified WiFi devices discovered within the viscinity
    
    * Add a new script named 'wifi-tshark-status-event.sh' to icinga2
      configuration directory. This script is responsible for publishing
      the WiFi device's status/presence information using MQTT, so that
      Home-Assistant can receive them - pretty cool!

--- configs/icinga2/scripts/wifi-tshark-status-event.sh
+++ configs/icinga2/scripts/wifi-tshark-status-event.sh
@@ -7,8 +7,8 @@ PROJECT_GLOBAL_SETTINGS_FILE=$PROJECT_SYSTEM_CONF_DIR/global-settings.cfg
 . $PROJECT_GLOBAL_SETTINGS_FILE
 #
 # Variables
-BLUEZ_MAC_ADDRESS=$(echo "$HOSTOUTPUT" | grep -oP "(?<=\[).+?(?=\])")
-MQTT_TOPIC_PRESENCE_BLUEZ=JambulaTV/house/presence/bluez/$BLUEZ_MAC_ADDRESS
+TSHARK_MAC_ADDRESS=$(echo "$HOSTOUTPUT" | grep -oP "(?<=\[).+?(?=\])")
+MQTT_TOPIC_PRESENCE_TSHARK=JambulaTV/house/presence/tshark/$TSHARK_MAC_ADDRESS
 
 
 # Quit if type of state alert is 'SOFT'
@@ -17,8 +17,8 @@ MQTT_TOPIC_PRESENCE_BLUEZ=JambulaTV/house/presence/bluez/$BLUEZ_MAC_ADDRESS
 # Publish status via MQTT
 if [[ "$HOSTSTATE" = "UP" ]];
 then
-$MQTT_PUBLISH_CMD $MQTT_PUBLISH_OPTS -t "$MQTT_TOPIC_PRESENCE_BLUEZ" -m "Home"
+$MQTT_PUBLISH_CMD $MQTT_PUBLISH_OPTS -t "$MQTT_TOPIC_PRESENCE_TSHARK" -m "Home"
 
 else
-$MQTT_PUBLISH_CMD $MQTT_PUBLISH_OPTS -t "$MQTT_TOPIC_PRESENCE_BLUEZ" -m "Away"
+$MQTT_PUBLISH_CMD $MQTT_PUBLISH_OPTS -t "$MQTT_TOPIC_PRESENCE_TSHARK" -m "Away"
 fi
